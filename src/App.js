051cebed38849c735ae70d62767e4a75e5f4e9ce
Ben Mooney <benjaminem3@gmail.com>
2023-01-21 15:13:31 -0500

    Refactor of components and pages structure, reorganized project's file structure, added new components for better maintainability

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import MovieInfoBox from './components/MovieInfoBox';
+import Home from './components/Home';
+import Header from './components/Header';
+import PopularMovies from './components/PopularMovies';
+import RouteSwitch from './RouteSwitch';
 
 function App() {
+  // const [movies, setMovies] = useState([]);
+
+  // useEffect(() => {
+  //   const movieData = async () => {
+  //     const res = await fetch(API_URL);
+  //     const data = await res.json();
+  //     console.log(data);
+  //     setMovies(data.results);
+  //   };
+  //   movieData();
+
+  //   // fetch(API_URL)
+  //   //   .then((res) => res.json())
+  //   //   .then((data) => {
+  //   //     setMovies(data.results);
+  //   //   });
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <RouteSwitch />
+
+      {/* <PopularMovies /> */}
     </div>
   );
 }
